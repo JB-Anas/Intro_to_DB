55402e2b7de2e14dfb12591e22b5454086fb93d4
JB-Anas <anaselljabiri@gmail.com>
2025-02-07 21:53:43 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -2,5 +2,5 @@
 USE alx_book_store;
 
 -- Insert a single row into the CUSTOMERS table
-INSERT INTO CUSTOMERS (CUSTOMER_NAME, EMAIL, ADDRESS)  
+INSERT INTO customer (CUSTOMER_NAME, EMAIL, ADDRESS)  
 VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
