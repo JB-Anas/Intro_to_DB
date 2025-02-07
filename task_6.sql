b20e6d7e5168cb88c87271765e61244037cced9d
JB-Anas <anaselljabiri@gmail.com>
2025-02-07 21:59:14 +0100

    Create task_6.sql

---
+++ task_6.sql
@@ -0,0 +1,9 @@
+-- use the target database
+use alx_book_store;
+
+-- insert multiple rows into the customers table
+insert into customers (customer_name, email, address)  
+values 
+    ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    ('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    ('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
